27cab7d9188367465a8299c53a2602873aa85748
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-09-30 21:28:07 -0300

    Created Real Estate parse logic

--- index.js
+++ index.js
@@ -22,17 +22,12 @@ async function parseUserModuleData(message) {
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
-    let columns = Object.keys(data);
+    const columns = Object.keys(data);
     const values = Object.values(data);
      request.input('id_usuario', sql.Int, values[0]);
      request.input('nombre', sql.VarChar, values[1]);
      request.input('tipo_usuario', sql.VarChar, values[2]);
      request.input('fecha_registro', sql.Date, new Date(values[3]));
-
-     console.log(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_usuario, @nombre, @tipo_usuario, @fecha_registro)
-     `)
      // Execute the query
      const result = await request.query(`
          INSERT INTO ${tableName} (${columns.join(', ')})
@@ -45,7 +40,44 @@ async function parseUserModuleData(message) {
   }
 }
 async function parseRealEstateModuleData(message) {
+  try {
+    const tableName = "raw_"+message.module_id;
+    const data = message.data;
+    // Validar que el nombre de la tabla no contenga caracteres peligrosos
+    if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
+      console.log('Invalid table name format' );
+    }
 
+    // Conexión a la base de datos
+    const pool = await config.poolPromise;
+    // Preparar la solicitud SQL
+    const request = pool.request();
+     // Parameters
+    const columns = Object.keys(data);
+    const values = Object.values(data);
+     request.input('id_publicacion', sql.Int, values[0]);
+     request.input('fecha_publicacion', sql.Date, new Date(values[1]));
+     request.input('precio_publicacion', sql.Decimal, values[2]);
+     request.input('tipo_publicacion', sql.VarChar, values[3]);
+     request.input('barrio', sql.VarChar, values[4]);
+     request.input('latitud', sql.Decimal, values[5]);
+     request.input('longitud', sql.Decimal, values[6]);
+     request.input('id_usuario', sql.Int, values[7]);
+
+     console.log(`
+         INSERT INTO ${tableName} (${columns.join(', ')})
+         VALUES (@id_publicacion, @fecha_publicacion, @precio_publicacion, @tipo_publicacion, @barrio, @latitud, @longitud, @id_usuario)
+     `)
+     // Execute the query
+     const result = await request.query(`
+         INSERT INTO ${tableName} (${columns.join(', ')})
+         VALUES (@id_publicacion, @fecha_publicacion, @precio_publicacion, @tipo_publicacion, @barrio, @latitud, @longitud, @id_usuario)
+     `);
+
+    console.log('Data inserted successfully', result );
+  } catch (error) {
+    console.error('SQL error', error);
+  }
 }
 async function parseAccountabilityModuleData(message) {
 
@@ -75,7 +107,6 @@ async function deleteMessages(messages){
         console.log("Message " + message.ReceiptHandle + " deleted successfully.")
       }
     })
-
   }
 }
 
@@ -84,28 +115,33 @@ async function processMessages(messages){
 
   for (let index = 0; index < messages.length; index++) {
     const messageString = messages[index];
-    const message = JSON.parse(messageString.Body);
-    switch (message.module_id) {
-      case 'usuarios':
-        parseUserModuleData(message)
-        break;
-      case 'publicaciones':
-        parseRealEstateModuleData(message)
-        break;
-      case 'pagos':
-        parsePaymentsModuleData(message)
-        break;
-      case 'mudanzas':
-        parseLogisticsModuleData(message)
-        break;
-      case 'financiamientos':
-        parseAccountabilityModuleData(message)
-        break;
-      case 'contratos':
-        parseLegalsModuleData(message)
-        break;
-      default:
-        break;
+    try{
+      const message = JSON.parse(messageString.Body);
+      switch (message.module_id) {
+        case 'usuarios':
+          parseUserModuleData(message)
+          break;
+        case 'publicaciones':
+          parseRealEstateModuleData(message)
+          break;
+        case 'pagos':
+          parsePaymentsModuleData(message)
+          break;
+        case 'mudanzas':
+          parseLogisticsModuleData(message)
+          break;
+        case 'financiamientos':
+          parseAccountabilityModuleData(message)
+          break;
+        case 'contratos':
+          parseLegalsModuleData(message)
+          break;
+        default:
+          break;
+      }
+    }
+    catch (err) {
+      console.error("Unable to process message: ", err)
     }
   }
 }
