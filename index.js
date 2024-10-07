d1a594ea44edea3d97ae35722e0fc2d1f20c5835
Stefano De Vita <devitastefano@hotmail.com>
2024-10-07 18:15:57 -0300

    added health check

--- index.js
+++ index.js
@@ -329,7 +329,20 @@ setInterval(()=>{
 }, 20000);
 
 
+app.get('/health', (req, res) => {
+  const healthCheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now()
+  };
 
+  try {
+    res.status(200).send(healthCheck);
+  } catch (error) {
+    healthCheck.message = error;
+    res.status(503).send();
+  }
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
