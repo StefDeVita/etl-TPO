d770fea54fe3b34faf3d3f2325fd00e09867db1b
Nach0Zar <ignaciozarlenga@hotmail.com>
2024-11-18 13:37:23 -0300

    Refactored so it deletes the message only after a successful processing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { sql } from './config/db.js';
 const app = express();
 app.use(express.json());
 const sqs = new config.AWS.SQS();
-async function parseUserModuleData(message) {
+async function parseUserModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -14,30 +14,26 @@ async function parseUserModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_usuario', sql.Int, values[0]);
-     request.input('nombre', sql.VarChar, values[1]);
-     request.input('tipo_usuario', sql.VarChar, values[2]);
-     request.input('fecha_registro', sql.Date, new Date(values[3]));
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_usuario, @nombre, @tipo_usuario, @fecha_registro)
-     `);
-
+    request.input('id_usuario', sql.Int, values[0]);
+    request.input('nombre', sql.VarChar, values[1]);
+    request.input('tipo_usuario', sql.VarChar, values[2]);
+    request.input('fecha_registro', sql.Date, new Date(values[3]));
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_usuario, @nombre, @tipo_usuario, @fecha_registro)
+    `);
     console.log('Data inserted successfully');
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function parseRealEstateModuleData(message) {
+async function parseRealEstateModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -45,39 +41,33 @@ async function parseRealEstateModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
-    // Preparar la solicitud SQL
-    const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_publicacion', sql.Int, values[0]);
-     request.input('fecha_publicacion', sql.Date, new Date(values[1]));
-     request.input('precio_publicacion', sql.Decimal, values[2]);
-     request.input('direccion', sql.VarChar, values[3]);
-     request.input('habitaciones', sql.Int, values[4]);
-     request.input('barrio', sql.VarChar, values[5]);
-     request.input('latitud', sql.Decimal, values[6]);
-     request.input('longitud', sql.Decimal, values[7]);
-     request.input('estado', sql.VarChar, values[8]);
-     request.input('id_usuario', sql.Int, values[9]);
-     request.input('tipo', sql.VarChar, values[10]);
-     request.input('superficie_total_m2', sql.Int, values[11]);
-     request.input('ganancia_generada', sql.Decimal, values[12]);
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_publicacion, @fecha_publicacion, @precio_publicacion, @direccion, @habitaciones, @barrio, @latitud, @longitud, @estado, @id_usuario, @tipo, @superficie_total_m2, @ganancia_generada)
-     `);
-
+    request.input('id_publicacion', sql.Int, values[0]);
+    request.input('fecha_publicacion', sql.Date, new Date(values[1]));
+    request.input('precio_publicacion', sql.Decimal, values[2]);
+    request.input('direccion', sql.VarChar, values[3]);
+    request.input('habitaciones', sql.Int, values[4]);
+    request.input('barrio', sql.VarChar, values[5]);
+    request.input('latitud', sql.Decimal, values[6]);
+    request.input('longitud', sql.Decimal, values[7]);
+    request.input('estado', sql.VarChar, values[8]);
+    request.input('id_usuario', sql.Int, values[9]);
+    request.input('tipo', sql.VarChar, values[10]);
+    request.input('superficie_total_m2', sql.Int, values[11]);
+    request.input('ganancia_generada', sql.Decimal, values[12]);
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_publicacion, @fecha_publicacion, @precio_publicacion, @direccion, @habitaciones, @barrio, @latitud, @longitud, @estado, @id_usuario, @tipo, @superficie_total_m2, @ganancia_generada)
+    `);
     console.log('Data inserted successfully' );
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function parseAccountabilityModuleData(message) {
+async function parseAccountabilityModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -85,32 +75,28 @@ async function parseAccountabilityModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_financiamiento', sql.Int, values[0]);
-     request.input('fecha_solicitud', sql.Date, new Date(values[1]));
-     request.input('monto_solicitado', sql.Decimal, values[2]);
-     request.input('monto_aprobado', sql.Decimal, values[3]);
-     request.input('estado_solicitud', sql.VarChar, values[4]);
-     request.input('id_usuario', sql.Int, values[5]);
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_financiamiento, @fecha_solicitud, @monto_solicitado, @monto_aprobado, @estado_solicitud, @id_usuario)
-     `);
-
+    request.input('id_financiamiento', sql.Int, values[0]);
+    request.input('fecha_solicitud', sql.Date, new Date(values[1]));
+    request.input('monto_solicitado', sql.Decimal, values[2]);
+    request.input('monto_aprobado', sql.Decimal, values[3]);
+    request.input('estado_solicitud', sql.VarChar, values[4]);
+    request.input('id_usuario', sql.Int, values[5]);
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_financiamiento, @fecha_solicitud, @monto_solicitado, @monto_aprobado, @estado_solicitud, @id_usuario)
+    `);
     console.log('Data inserted successfully' );
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function parsePaymentsModuleData(message) {
+async function parsePaymentsModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -118,32 +104,28 @@ async function parsePaymentsModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_pago', sql.Int, values[0]);
-     request.input('fecha', sql.Date, new Date(values[1]));
-     request.input('monto', sql.Decimal, values[2]);
-     request.input('id_publicacion', sql.Int, values[3]);
-     request.input('id_usuario', sql.Int, values[4]);
-     request.input('estado', sql.VarChar, values[5]);
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_pago, @fecha, @monto, @id_publicacion, @id_usuario, @estado)
-     `);
-
+    request.input('id_pago', sql.Int, values[0]);
+    request.input('fecha', sql.Date, new Date(values[1]));
+    request.input('monto', sql.Decimal, values[2]);
+    request.input('id_publicacion', sql.Int, values[3]);
+    request.input('id_usuario', sql.Int, values[4]);
+    request.input('estado', sql.VarChar, values[5]);
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_pago, @fecha, @monto, @id_publicacion, @id_usuario, @estado)
+    `);
     console.log('Data inserted successfully' );
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function parseLegalsModuleData(message) {
+async function parseLegalsModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -151,37 +133,33 @@ async function parseLegalsModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_contrato', sql.Int, values[0]);
-     request.input('id_publicacion', sql.Int, values[1]);
-     request.input('id_usuario_locatario', sql.Int, values[2]);
-     request.input('id_usuario_locador_o_mudanza', sql.Int, values[3]);
-     request.input('id_usuario_escribano', sql.Int, values[4]);
-     request.input('tipo_contrato', sql.VarChar, values[5]);
-     request.input('fecha_firma', sql.Date, new Date(values[6]));
-     request.input('fecha_inicio', sql.Date, new Date(values[7]));
-     request.input('fecha_fin', sql.Date, new Date(values[8]));
-     request.input('monto', sql.Decimal, values[9]);
-     request.input('estado_contrato', sql.VarChar, values[10]);
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_contrato, @id_publicacion, @id_usuario_locatario, @id_usuario_locador_o_mudanza, @id_usuario_escribano, @tipo_contrato, @fecha_firma, @fecha_inicio, @fecha_fin, @monto, @estado_contrato)
-     `);
-
+    request.input('id_contrato', sql.Int, values[0]);
+    request.input('id_publicacion', sql.Int, values[1]);
+    request.input('id_usuario_locatario', sql.Int, values[2]);
+    request.input('id_usuario_locador_o_mudanza', sql.Int, values[3]);
+    request.input('id_usuario_escribano', sql.Int, values[4]);
+    request.input('tipo_contrato', sql.VarChar, values[5]);
+    request.input('fecha_firma', sql.Date, new Date(values[6]));
+    request.input('fecha_inicio', sql.Date, new Date(values[7]));
+    request.input('fecha_fin', sql.Date, new Date(values[8]));
+    request.input('monto', sql.Decimal, values[9]);
+    request.input('estado_contrato', sql.VarChar, values[10]);
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_contrato, @id_publicacion, @id_usuario_locatario, @id_usuario_locador_o_mudanza, @id_usuario_escribano, @tipo_contrato, @fecha_firma, @fecha_inicio, @fecha_fin, @monto, @estado_contrato)
+    `);
     console.log('Data inserted successfully' );
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function parseLogisticsModuleData(message) {
+async function parseLogisticsModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -189,38 +167,33 @@ async function parseLogisticsModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_mudanza', sql.Int, values[0]);
-     request.input('fecha_solicitud', sql.Date, new Date(values[1]));
-     request.input('fecha_realizacion', sql.Date, new Date(values[2]));
-     request.input('costo_mudanza', sql.Decimal, values[3]);
-     request.input('barrio_origen', sql.VarChar, values[4]);
-     request.input('barrio_destino', sql.VarChar, values[5]);
-     request.input('latitud_origen', sql.Decimal, values[6]);
-     request.input('longitud_origen', sql.Decimal, values[7]);
-     request.input('latitud_destino', sql.Decimal, values[8]);
-     request.input('longitud_destino', sql.Decimal, values[9]);
-     request.input('id_usuario', sql.Int, values[10]);
-
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_mudanza, @fecha_solicitud, @fecha_realizacion, @costo_mudanza, @barrio_origen, @barrio_destino, @latitud_origen, @longitud_origen, @latitud_destino, @longitud_destino, @id_usuario)
-     `);
-
+    request.input('id_mudanza', sql.Int, values[0]);
+    request.input('fecha_solicitud', sql.Date, new Date(values[1]));
+    request.input('fecha_realizacion', sql.Date, new Date(values[2]));
+    request.input('costo_mudanza', sql.Decimal, values[3]);
+    request.input('barrio_origen', sql.VarChar, values[4]);
+    request.input('barrio_destino', sql.VarChar, values[5]);
+    request.input('latitud_origen', sql.Decimal, values[6]);
+    request.input('longitud_origen', sql.Decimal, values[7]);
+    request.input('latitud_destino', sql.Decimal, values[8]);
+    request.input('longitud_destino', sql.Decimal, values[9]);
+    request.input('id_usuario', sql.Int, values[10]);
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_mudanza, @fecha_solicitud, @fecha_realizacion, @costo_mudanza, @barrio_origen, @barrio_destino, @latitud_origen, @longitud_origen, @latitud_destino, @longitud_destino, @id_usuario)
+    `);
     console.log('Data inserted successfully' );
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function parseTicketsModuleData(message) {
+async function parseTicketsModuleData(pool, message) {
   try {
     const tableName = "raw_"+message.module_id;
     const data = message.data;
@@ -228,50 +201,43 @@ async function parseTicketsModuleData(message) {
     if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
       console.log('Invalid table name format' );
     }
-
-    // Conexión a la base de datos
-    const pool = await config.poolPromise;
     // Preparar la solicitud SQL
     const request = pool.request();
      // Parameters
     const columns = Object.keys(data);
     const values = Object.values(data);
-     request.input('id_reclamo', sql.Int, values[0]);
-     request.input('fecha__reclamo', sql.Date, new Date(values[1]));
-     request.input('estado', sql.VarChar, values[2]);
-     request.input('id_usuario', sql.Int, values[3]);
-     request.input('categoria', sql.VarChar, values[4]);
-     // Execute the query
-     const result = await request.query(`
-         INSERT INTO ${tableName} (${columns.join(', ')})
-         VALUES (@id_reclamo, @fecha__reclamo, @estado, @id_usuario, @categoria)
-     `);
-
+    request.input('id_reclamo', sql.Int, values[0]);
+    request.input('fecha__reclamo', sql.Date, new Date(values[1]));
+    request.input('estado', sql.VarChar, values[2]);
+    request.input('id_usuario', sql.Int, values[3]);
+    request.input('categoria', sql.VarChar, values[4]);
+    // Execute the query
+    await request.query(`
+        INSERT INTO ${tableName} (${columns.join(', ')})
+        VALUES (@id_reclamo, @fecha__reclamo, @estado, @id_usuario, @categoria)
+    `);
     console.log('Data inserted successfully' );
   } catch (error) {
     console.error('SQL error', error);
   }
 }
-async function deleteMessages(messages){
-  for (let index  = 0; index < messages.length; index++) {
-    let message = messages[index];
-    const deleteParams = {
-      QueueUrl: config.AWS_SQS_QUEUE_URL,
-      ReceiptHandle: message.ReceiptHandle
-    }
-    sqs.deleteMessage(deleteParams, (err, data) => {
-      if (err) {
-        console.error("Error", err);
-      }
-      else {
-        console.log("Message " + "deleted successfully.")
-      }
-    })
+function deleteMessage(message){
+  const deleteParams = {
+    QueueUrl: config.AWS_SQS_QUEUE_URL,
+    ReceiptHandle: message.ReceiptHandle
   }
+  sqs.deleteMessage(deleteParams, (err, data) => {
+    if (err) {
+      console.error("Error", err);
+    }
+    else {
+      console.log("Message " + message.ReceiptHandle + "deleted successfully.")
+    }
+  })
 }
 
 
-async function processMessages(messages){
+async function processMessages(pool, messages){
 
   for (let index = 0; index < messages.length; index++) {
     const messageString = messages[index];
@@ -279,29 +245,30 @@ async function processMessages(messages){
       const message = JSON.parse(messageString.Body);
       switch (message.module_id) {
         case 'usuarios':
-          parseUserModuleData(message)
+          parseUserModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         case 'publicaciones':
-          parseRealEstateModuleData(message)
+          parseRealEstateModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         case 'pagos':
-          parsePaymentsModuleData(message)
+          parsePaymentsModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         case 'mudanzas':
-          parseLogisticsModuleData(message)
+          parseLogisticsModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         case 'financiamientos':
-          parseAccountabilityModuleData(message)
+          parseAccountabilityModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         case 'contratos':
-          parseLegalsModuleData(message)
+          parseLegalsModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         case 'reclamos':
-          parseTicketsModuleData(message)
+          parseTicketsModuleData(pool, message).then(() => deleteMessage(messageString))
           break;
         default:
           break;
       }
+      deleteMessage(messageString);
     }
     catch (err) {
       console.error("Unable to process message: ", err)
@@ -311,23 +278,24 @@ async function processMessages(messages){
 
 setInterval(()=>{
   sqs.receiveMessage({
-    QueueUrl: config.AWS_SQS_QUEUE_URL, // Replace with your SQS queue URL
+    QueueUrl: config.AWS_SQS_QUEUE_URL,
     WaitTimeSeconds: 20,
     MaxNumberOfMessages: 10
-  }, (err, data) => {
+  }, async (err, data) => {
     if (err) {
       console.error(err);
     } else {
       data.Messages.forEach(message => {
         console.log(JSON.parse(message.Body));
       });
+      // Conexión a la base de datos
+      const pool = await config.poolPromise;
+      console.log(pool.connected)
       // Process the received messages here
-      processMessages(data.Messages);
-      deleteMessages(data.Messages);
+      processMessages(pool, data.Messages);
     }
   });
 }, 20000);
-
 
 app.get('/health', (req, res) => {
   const healthCheck = {
